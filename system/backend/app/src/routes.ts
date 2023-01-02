17421d34ac69aa0b5efa779e188bafe10ab10bb6
diederik <diederik@MacBook-Pro-2015.local>
2023-01-02 16:13:46 +0100

    setup db

--- system/backend/app/src/routes.ts
+++ system/backend/app/src/routes.ts
@@ -1,9 +1,35 @@
 import { Router, Response } from 'express'
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgresadmin',
+  host: 'postgres',
+  database: 'kube-full-db',
+  password: 'admin123',
+  port: 5432,
+})
+
 const routes = Router()
 
-routes.get('/', (_, res: Response) => res.json({ 
-  	message: `Hai, from the server :D (fronturl: ${process.env.FRONTEND_URL})` 
-}))
+routes.get('/', async (_, res: Response) => {
+	return res.json({ 
+  		message: `Hai, from the server :D (fronturl: ${process.env.FRONTEND_URL})` 
+	})
+})
+
+routes.get('/products', async (_, res: Response) => {
+	try {
+		pool.query('SELECT * FROM product', (error: any, results: any) => {
+			if (error) {
+			  throw error
+			}
+			return res.json({ 
+				result: results.rows
+			})
+		})
+	} catch (e){
+		console.log(e)
+	}
+})
 
 export default routes
