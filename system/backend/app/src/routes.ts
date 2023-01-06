3a4566312f2949d128f7021f034cbf7636239879
diederik <diederik@MacBook-Pro-2015.local>
2023-01-06 17:27:23 +0100

    fixed dev and prod environment with added database

--- system/backend/app/src/routes.ts
+++ system/backend/app/src/routes.ts
@@ -2,11 +2,11 @@ import { Router, Response } from 'express'
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgresadmin',
-  host: 'postgres',
-  database: 'kube-full-db',
-  password: 'admin123',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 })
 
 const routes = Router()
