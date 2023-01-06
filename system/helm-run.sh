809dadb8e13d23c29e0abf6431240bfdbe540d07
diederik <diederik@MacBook-Pro-2015.local>
2023-01-06 17:56:11 +0100

    setup

--- system/helm-run.sh
+++ system/helm-run.sh
@@ -1,7 +1,9 @@
 # delete old helm charts if exists
 microk8s helm3 uninstall frontend-app 
 microk8s helm3 uninstall backend-app 
+microk8s helm3 uninstall db-app 
 
 # create new helm charts
 microk8s helm3 install backend-app ./backend/helm-backend
 microk8s helm3 install frontend-app ./frontend/helm-frontend
+microk8s helm3 install db-app ./db/helm-chart
